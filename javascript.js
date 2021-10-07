add14e7197b5ab7306cab71f04daab420c57f73a
sean-siddells <seansiddells@gmail.com>
2021-10-07 18:54:05 +1300

    add listener to foul buttons

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   bindEventListeners(document.getElementsByClassName('balls'))
   bindButtons(document.getElementById('button'))
+  bindFouls(document.getElementsByClassName('fouls'))
 }
 
 function bindEventListeners (balls) {
@@ -15,8 +16,14 @@ function bindButtons (button) {
        button.addEventListener('click', changePlayer)
 } 
 
-const ballArr = ['red','yellow','green','brown','blue', 'pink','black']
+function bindFouls (foulButtons) {
+    for(var i= 0; i < foulButtons.length; i++) {
+        foulButtons[i].addEventListener('click', addFoul)
+    }
+}
 
+const ballArr = ['red','yellow','green','brown','blue', 'pink','black']
+const fouls = ['7','6','5','4']
 // player 1 stats are stats[0], player 2 stats are stats[1]
 
 var stats =[]
@@ -57,8 +64,6 @@ stats = [
     }
 ]
 
-console.log('starting fouls for player one count is: ', stats[0]['fouls'])
-
 function changePlayer () {
     if (document.getElementById('currentPlayer').innerHTML === 'Player 1') {
         document.getElementById('currentPlayer').innerHTML = 'Player 2'
@@ -71,7 +76,6 @@ function changePlayer () {
 // function with if statement in updateCounts that if the global object variable is different from player's currentfouls, 
 //then update other players score by the 
 // difference of each foul count. otherwise do nothing.
-
 var fouls1 = {
     '7':0,
     '6':0,
@@ -91,16 +95,12 @@ function foulUpdate (playernum) {
         if (fouls1 === stats[0]['fouls']) {
             return
         } else if (fouls1 !== stats[0]['fouls']) {
-            stats[1]['totalScore'] += 7*(fouls1['7'] - stats[0]['fouls']['7']) + 6*(fouls1['6'] - stats[0]['fouls']['6']) 
-                                      + 5*(fouls1['5'] - stats[0]['fouls']['5']) + 4*(fouls1['4'] - stats[0]['fouls']['4'])
             stats[0]['fouls'] = fouls1
         }
     } else if (playernum === 2) {
         if (fouls2 === stats[1]['fouls']) {
             return
         } else if (fouls2 !== stats[1]['fouls']) {
-            stats[0]['totalScore'] += 7*(fouls2['7'] - stats[1]['fouls']['7']) + 6*(fouls2['6'] - stats[1]['fouls']['6']) 
-                                      + 5*(fouls2['5'] - stats[1]['fouls']['5']) + 4*(fouls2['4'] - stats[1]['fouls']['4'])
             stats[1]['fouls'] = fouls2
             console.log (stats)
         }
@@ -111,6 +111,7 @@ function foulUpdate (playernum) {
 //calls the countScore function to update the totalscore being displayed
 
 function updateStats (evt) {
+    console.log('updateStats is working!')
     var player = document.getElementById('currentPlayer').innerHTML
 
    if (player === 'Player 1') {
@@ -143,21 +144,36 @@ function updateStats (evt) {
         otherArrayNum = 0
    } 
 
-   console.log('other player is: ', otherPlayer)
    otherPlayer = otherPlayer.join('')
    otherPlayer = otherPlayer.toLowerCase()
    foulUpdate(number)
    countScore(ballArr, stats, number)
    document.getElementById(lowerPlayer + 'Score').innerHTML = stats[arrayNum]['totalScore']
    document.getElementById(otherPlayer + 'Score').innerHTML = stats[otherArrayNum]['totalScore']
-//    document.getElementById(otherPlayer + 'Score').innerHTML = stats[otherArrayNum]['totalScore']
-    console.log (stats[0])
-    console.log (stats[1])
-    console.log ('updated player one fouls count is: ', stats[0]['fouls'])
+   console.log('player1 stats: ', stats[0])
+   console.log('player2 stats: ', stats[1])
+}
+
+
+function addFoul(evt) {
+    console.log('this is the event target id: ', evt.target.id)
+    if (document.getElementById('currentPlayer').innerHTML === 'Player 1') {
+        for (var i =0; i < fouls.length; i++) {
+            if (evt.target.id === fouls[i]) {
+                fouls1[fouls[i]]++
+            } 
+         }
+    } else {
+        for (var i =0; i < fouls.length; i++) {
+            if (evt.target.id === fouls[i]) {
+                fouls2[fouls[i]]++
+            } 
+        }  
+    }
+    updateStats(evt)
 }
 
 //function that passes the ballArr array as an argument and returns the corresponding values for each ball
-
 function ballValue(colour) {
     for (var i = 0; i < ballArr.length; i++) {
         if (colour === ballArr[i]) {
@@ -166,18 +182,33 @@ function ballValue(colour) {
     }
 }
 
-//total score from balls, deducts the total fouls and assigns it to player(1 or 2)score to be displayed
-//still needs to have the value of the corresponding balls * the value and fouls * value, atm is just the counts
+//total score from balls, adds the total fouls and assigns it to player(1 or 2)score to be displayed 
 
-function countScore(arr, stat, num) {
-    var playerScore = 0
- 
-    for (var i = 0; i < arr.length; i++) {
+function countScore(ballArr, stat, num) {
+
+    function otherPlayer () {
         if (num === 1) {
-            playerScore += ballValue(arr[i]) * stat[0][arr[i]];
+            return 1
         } else if (num === 2) {
-            playerScore += ballValue(arr[i]) * stat[1][arr[i]];
+            return 0
+        }
+    }
+
+    var playerScore = 0
+    var otherPlayerScore = stats[otherPlayer(num)]['totalScore']
+
+    for (var i = 0; i < ballArr.length; i++) {
+        if (num === 1) {
+            playerScore += ballValue(ballArr[i]) * stat[0][ballArr[i]];
+        } else if (num === 2) {
+            playerScore += ballValue(ballArr[i]) * stat[1][ballArr[i]];
         } 
+    }
+
+    if (num === 1) {
+        otherPlayerScore += 7*(fouls1['7'] - stats[0]['fouls']['7']) + 6*(fouls1['6'] - stats[0]['fouls']['6']) 
+                            + 5*(fouls1['5'] - stats[0]['fouls']['5']) + 4*(fouls1['4'] - stats[0]['fouls']['4'])
+        stats[otherPlayer(num)]['totalScore'] = otherPlayerScore
     }
 
     if (num === 1) {
